45a38e5f30138c9b0964e70082d16a7ded659fa1
Jean-François Swain <swain.jean@gmail.com>
2020-02-10 18:48:18 +0100

    [MOD] (cart) Create page

--- components/Shopping.vue
+++ components/Shopping.vue
@@ -6,7 +6,9 @@
 
     <span v-if="shoppingQuantity === 0" class="shopping-cart__amount">0</span>
 
-    <p class="shopping-cart__name">shopping-cart</p>
+    <nuxt-link to="/cart">
+      <p class="shopping-cart__name">shopping-cart</p>
+    </nuxt-link>
 
     <img src="@/assets/images/shopping-cart-icon.svg" alt />
   </div>
@@ -47,6 +49,10 @@ export default {
     @include breakpoint('beyond-medium') {
       margin-right: 0;
     }
+  }
+
+  a {
+    text-decoration: none;
   }
 
   &__name {
---
+++ pages/cart/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <article>
+      <header>
+        <h2>Cart List</h2>
+      </header>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+section {
+  max-width: $project-inner;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: $project-mobile-padding;
+  padding-right: $project-mobile-padding;
+
+  @include breakpoint(1052) {
+    padding-left: 0;
+    padding-right: 0;
+  }
+}
+
+h2 {
+  font-family: font-name('default-black');
+  font-size: rem(32);
+  margin-top: 4rem;
+
+  @include breakpoint('beyond-project') {
+    margin-top: 8.8rem;
+  }
+}
+</style>
