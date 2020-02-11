e7303524fca12db07a3ecf8a5089bdbeb0c06990
Jean-François Swain <swain.jean@gmail.com>
2020-02-11 18:26:38 +0100

    [MOD](payment) Create payment apge

---
+++ pages/cart/payment.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <shopping-order :step="2" />
+
+    <article>
+      <header>
+        <h2>Payment type</h2>
+        <p>Choose how do you want to pay your order</p>
+      </header>
+    </article>
+  </section>
+</template>
+
+<script>
+import ShoppingOrder from '@/components/ShoppingOrder'
+
+export default {
+  components: {
+    ShoppingOrder
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
